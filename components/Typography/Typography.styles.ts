a9e2acee2330e8914946aaa6c6ccfc718750b8f1
Hina0222 <hina0222@naver.com>
2025-03-16 20:59:43 +0900

    feat: Typography 공용 컴포넌트화

---
+++ components/Typography/Typography.styles.ts
@@ -0,0 +1,99 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	H1: {
+		fontSize: 32,
+		fontWeight: "700",
+		lineHeight: 44
+	},
+	H2: {
+		fontSize: 28,
+		fontWeight: "700",
+		lineHeight: 36
+	},
+	H3: {
+		fontSize: 24,
+		fontWeight: "700",
+		lineHeight: 32
+	},
+	T1: {
+		fontSize: 24,
+		fontWeight: "600",
+		lineHeight: 32
+	},
+	T2_bold: {
+		fontSize: 20,
+		fontWeight: "700",
+		lineHeight: 28
+	},
+	T2_semibold: {
+		fontSize: 20,
+		fontWeight: "600",
+		lineHeight: 28
+	},
+	T3_bold: {
+		fontSize: 18,
+		fontWeight: "700",
+		lineHeight: 24
+	},
+	T3_semibold: {
+		fontSize: 18,
+		fontWeight: "600",
+		lineHeight: 24
+	},
+	T3_medium: {
+		fontSize: 18,
+		fontWeight: "500",
+		lineHeight: 24
+	},
+	T4_bold: {
+		fontSize: 16,
+		fontWeight: "700",
+		lineHeight: 24
+	},
+	T4_semibold: {
+		fontSize: 16,
+		fontWeight: "600",
+		lineHeight: 24
+	},
+	T4_medium: {
+		fontSize: 16,
+		fontWeight: "500",
+		lineHeight: 24
+	},
+	B1_bold: {
+		fontSize: 16,
+		fontWeight: "700",
+		lineHeight: 24
+	},
+	B1_semibold: {
+		fontSize: 16,
+		fontWeight: "600",
+		lineHeight: 24
+	},
+	B1_medium: {
+		fontSize: 16,
+		fontWeight: "500",
+		lineHeight: 24
+	},
+	B2_semibold: {
+		fontSize: 14,
+		fontWeight: "600",
+		lineHeight: 20
+	},
+	B2_medium: {
+		fontSize: 14,
+		fontWeight: "500",
+		lineHeight: 20
+	},
+	B3_semibold: {
+		fontSize: 12,
+		fontWeight: "600",
+		lineHeight: 16
+	},
+	B3_medium: {
+		fontSize: 12,
+		fontWeight: "500",
+		lineHeight: 16
+	}
+});
---
+++ components/Typography/Typography.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+import { Text } from "react-native";
+import { Color } from "../../types/color";
+import { theme } from "../../styles/theme";
+import { styles } from "./Typography.styles";
+
+type TypographyType =
+	| "H1"
+	| "H2"
+	| "H3"
+	| "T1"
+	| "T2_bold"
+	| "T2_semibold"
+	| "T3_bold"
+	| "T3_semibold"
+	| "T3_medium"
+	| "T4_bold"
+	| "T4_semibold"
+	| "T4_medium"
+	| "B1_bold"
+	| "B1_semibold"
+	| "B1_medium"
+	| "B2_semibold"
+	| "B2_medium"
+	| "B3_semibold"
+	| "B3_medium";
+
+interface TypographyPropsType {
+	color: Color;
+	size: TypographyType;
+	children: ReactNode;
+}
+
+export default function Typography({ color, size, children }: TypographyPropsType) {
+	const Typo = styles[size];
+	const Color = theme[color as Color];
+
+	return (
+		<Text style={[Typo, { color: Color }]}>
+			{children}
+		</Text>
+	);
+}
